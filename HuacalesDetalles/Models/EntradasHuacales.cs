390a7ef756a64bec2cb365a1b1e71664e255a773
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-02-27 21:24:05 -0400

    Agrega los Modelos.

---
+++ HuacalesDetalles/Models/EntradasHuacales.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace HuacalesDetalles.Models;
+public class EntradasHuacales
+{
+    [Key]
+    public int EntradaId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string Nombre { get; set; } = string.Empty;
+    public ICollection<DetalleHuacales> ListaHuacales { get; set; } = new List<DetalleHuacales>();
+}
